635bc9353481b647b964e290e1fc3750029dd5b3
AnastasiaPanysh <nastya.panysh@gmail.com>
2023-03-02 21:57:17 +0300

    add preview advantages

---
+++ src/components/Features/Features.js
@@ -0,0 +1,27 @@
+import style from './Features.module.css'
+import { useState } from 'react'
+import ContentItem from './ContentItem';
+
+function Features() {
+    const [content, setContent] = useState(<ContentItem label={'Internal editor'} />);
+
+    function getContent(event) {
+        setContent(<ContentItem label={event.target.textContent} />)
+        console.log(event.target.textContent);
+    }
+
+    return (
+        <div className={style['wrapper']}>
+            <h2>Explore more features</h2>
+            <div onClick={getContent} className={style['container']}>
+                <p>Internal editor</p>
+                <p>Compact preview</p>
+                <p>Convenient search</p>
+                <p>Audio version</p>
+            </div>
+            <div className={style['block-feature']}> {content} </div>
+        </div>
+    )
+}
+
+export default Features
--- src/page/LandingPage/LandingPage.js
+++ src/page/LandingPage/LandingPage.js
@@ -1,11 +1,13 @@
 import Advantages from "../../components/Advantages/Advantages";
+import Features from "../../components/Features/Features";
 import Preview from "../../components/Preview/Preview"
 
 function LandingPage() {
     return (
         <>
-            <Preview/>
-            <Advantages/>
+            <Preview />
+            <Advantages />
+            <Features />
         </>
     )
 }
